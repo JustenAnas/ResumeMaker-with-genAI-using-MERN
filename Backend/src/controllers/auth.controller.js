389e4674550bc4ae38ece32ed754137180678d2d
Anas <khananas49340@gmail.com>
2026-03-21 02:05:59 +0530

    fix cookie settings for production

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -45,7 +45,11 @@ async function registerUserController(req,res) {
     },process.env.JWT_SECRET,
     {expiresIn:"1d"})
 
-    res.cookie("token",token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+})
 
     res.status(201).json({
         message:"user registered successfully",
@@ -82,7 +86,12 @@ async function loginUserController(req,res){
     },process.env.JWT_SECRET,
     {expiresIn:"1d"})
 
-    res.cookie("token",token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+})
+
      res.status(201).json({
         message:"user logged in successfully",
         user:{
