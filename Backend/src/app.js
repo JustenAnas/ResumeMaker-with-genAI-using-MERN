0208fb4e3c8aa390bb474be066d4fc79199dc9fa
Anas <khananas49340@gmail.com>
2026-03-21 02:01:54 +0530

    fix cors for production

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cookieParser())
 // express.urlencoded()
 app.use(cors({
-    origin:"http://localhost:5173",
+     origin: "https://resume-maker-with-gen-ai-using-mern.vercel.app",
     credentials: true
 }))
 
